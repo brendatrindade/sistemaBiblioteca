401200b33b4f426b88080d6859a9ee7c93bb3977
Brenda Trindade <142849685+brendatrindade@users.noreply.github.com>
2023-10-02 01:09:33 -0300

    :construction: Implementação da Classe Relatório
    Criação da Classe Relatorio para implementação futura de estatísticas.

---
+++ src/Model/Operacoes/Relatorio.java
@@ -0,0 +1,13 @@
+package Model.Operacoes;
+
+import java.util.List;
+
+public class Relatorio {
+    private long numLivrosEmprestados;
+    private long numLivrosAtrasados;
+    private long numLivrosReservados;
+    private List<Livro> livrosMaisPopulares;
+
+    //Em construção
+
+}
